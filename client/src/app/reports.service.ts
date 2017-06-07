fc4e9a53525806ec2c7866c1c4eba0b4db8da510
toggit <tog.spam.mail@gmail.com>
2017-06-08 01:02:17 +0300

    fix bracets

--- client/src/app/reports.service.ts
+++ client/src/app/reports.service.ts
@@ -41,12 +41,11 @@ export class ReportsService {
         if (this.geo.length > 0 && this.tag.length > 0) {
           this.findClosetGeo4Tag();
         }
-
         return data;
       }
       );
   }
-
+  
   findClosetGeo4Tag() {
     this.tag.forEach(
       (tag_elm, idx, arr) => {
@@ -57,22 +56,24 @@ export class ReportsService {
         this.geo.forEach(
           (geo_elm, idx, arr) => {
             geo_elm.label = 'G';
-            if (tag_elm.client_id === geo_elm.client_id) { }
-            if (tag_elm.match == false) {
-              const geo_time = new Date(geo_elm.time).getTime();
-              if (tag_time === geo_time) {
-                console.log("match found: client_id=" + tag_elm.client_id + "  geo_index=" + geo_elm.index)
-                tag_elm.match = true;
-              } else {
-                let diff = Math.abs(tag_time - geo_time);
-                if (tag_elm.diff > diff)
-                  tag_elm.diff = diff;
-                tag_elm.location = geo_elm.location;
-                tag_elm.geo_index = geo_elm.index;
+            if (tag_elm.client_id === geo_elm.client_id) {
+              if (tag_elm.match == false) {
+                const geo_time = new Date(geo_elm.time).getTime();
+                if (tag_time === geo_time) {
+                  console.log("match found: client_id=" + tag_elm.client_id + "  geo_index=" + geo_elm.index)
+                  tag_elm.match = true;
+                } else {
+                  let diff = Math.abs(tag_time - geo_time);
+                  if (tag_elm.diff > diff)
+                    tag_elm.diff = diff;
+                  tag_elm.location = geo_elm.location;
+                  tag_elm.geo_index = geo_elm.index;
+                }
               }
             }
           })
       }
     );
   }
+
 }
